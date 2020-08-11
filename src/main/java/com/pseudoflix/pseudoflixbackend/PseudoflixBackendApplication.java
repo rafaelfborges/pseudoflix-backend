45b9ea611247210b0d41306ef699127924ba3d8f
Rafael Borges <rafaelfborges@gmail.com>
2020-08-11 01:55:02 -0300

    Setup base project for backend with Spring

---
+++ src/main/java/com/pseudoflix/pseudoflixbackend/PseudoflixBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pseudoflix.pseudoflixbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PseudoflixBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PseudoflixBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pseudoflix/pseudoflixbackend/PseudoflixBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pseudoflix.pseudoflixbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PseudoflixBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
